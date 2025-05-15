f576eacb400827c6c4e28a99ac15eb5aac1e9353
Serega4x4 <serega1988bykov@gmail.com>
2025-05-15 17:36:02 +0200

    a little bit

--- app/Console/Commands/SendHello.php
+++ app/Console/Commands/SendHello.php
@@ -24,11 +24,12 @@ class SendHello extends Command
         $chatId = config('services.telegram.chat_id');
 
         $greetings = [
-            'Asia/Krasnoyarsk'     => 'Шуматкечын ямле, @Andriyanich85. Илышым куанен шупшылаш жап эртен. (Доброе утро, Сергей. Пора наслаждаться жизнью)',
-            'Asia/Omsk'            => '@Bessovestnotalantlivi, сәлам! Яңа үрләр яуларга вакыт. (Артур, салям! Пора покарять новые вершины)',
-            'Asia/Yekaterinburg'   => 'Salam aleykum, @KenanHM! Nətərsən? (Привет, Кенан! Как дела?)',
-            'Europe/Warsaw'        => '@Tamagochi4x4, здравствуйте... В Ваше отсутствие всё было в порядке! (Stwórco, cześć... Wszystko było w porządku, gdy Cię nie było.)',
-            'Europe/Berlin'        => 'Guten Morgen, @KAGBest! Zeit, in den neuen Tag zu starten. (Доброе утро, Артем Генадиевич! Пора вставать в новый день)',
+            // 'Asia/Krasnoyarsk'     => 'Шуматкечын ямле, @Andriyanich85. Илышым куанен шупшылаш жап эртен. (Доброе утро, Сергей. Пора наслаждаться жизнью)',
+            // 'Asia/Omsk'            => '@Bessovestnotalantlivi, сәлам! Яңа үрләр яуларга вакыт. (Артур, салям! Пора покарять новые вершины)',
+            // 'Asia/Yekaterinburg'   => 'Salam aleykum, @KenanHM! Nətərsən? (Привет, Кенан! Как дела?)',
+            // 'Europe/Warsaw'        => '@Tamagochi4x4, здравствуйте... В Ваше отсутствие всё было в порядке! (Stwórco, cześć... Wszystko było w porządku, gdy Cię nie było.)',
+            // 'Europe/Berlin'        => 'Guten Morgen, @KAGBest! Zeit, in den neuen Tag zu starten. (Доброе утро, Артем Генадиевич! Пора вставать в новый день)',
+            'Asia/Krasnoyarsk'     => 'Приветствую, Родня! Как спалось? Как настроение?',
         ];
 
         $message = $greetings[$timezone] ?? 'Доброе утро!';
