11e2b6cc9f5611acb6281409eee5be0411049da3
Serega4x4 <serega1988bykov@gmail.com>
2025-05-24 18:46:40 +0200

    e

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,7 +8,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
-        health: '/up',
+        health: '/api/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
