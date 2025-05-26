4bfe0bb4ac9bebbe236eae42d15cd2fd07bb7695
Serega4x4 <serega1988bykov@gmail.com>
2025-05-26 20:54:25 +0200

    change again without artisan

--- app/Console/Commands/SendDailyTelegramReport.php
+++ app/Console/Commands/SendDailyTelegramReport.php
@@ -3,7 +3,6 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use App\Http\Controllers\WeatherController;
 use App\Http\Controllers\MoneyController;
 use App\Service\Weather\Service;
 use Telegram\Bot\Api;
--- routes/api.php
+++ routes/api.php
@@ -92,9 +92,3 @@ Route::get('/run-scheduler', function (Request $request) {
 
     return response('Scheduler executed');
 });
-
-
-Route::get('/run-artisan/{cmd}', function ($cmd) {
-    Artisan::call($cmd);
-    return 'Done: ' . $cmd;
-});
