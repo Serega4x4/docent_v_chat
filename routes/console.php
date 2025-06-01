d1d48724c6363a6ec73488bae3415b0ee05d8f48
Serega4x4 <serega1988bykov@gmail.com>
2025-06-01 19:41:56 +0200

    мпываф

--- routes/console.php
+++ routes/console.php
@@ -50,6 +50,6 @@ foreach ($timezones as $timezone) {
 
     // Поздравления с днём рождения
     Schedule::command("telegram:happy-birthday {$timezone}")
-        ->dailyAt('22:30')
+        ->dailyAt('22:47')
         ->timezone($timezone);
 }
