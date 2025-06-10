2935487ac594c485f89f8709e85ade092767708d
Serega4x4 <serega1988bykov@gmail.com>
2025-06-10 10:54:08 +0200

    format

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,14 +18,15 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot()
     {
         if (app()->environment('production')) {
-        $credentials = env('GOOGLE_DRIVE_CREDENTIALS_JSON');
+            $credentials = env('GOOGLE_DRIVE_CREDENTIALS_JSON');
 
-        if ($credentials) {
-            File::ensureDirectoryExists(storage_path('app/google'));
-            File::put(storage_path('app/google/credentials.json'), $credentials);
+            if ($credentials) {
+                File::ensureDirectoryExists(storage_path('app/google'));
+                File::put(storage_path('app/google/credentials.json'), $credentials);
+            }
         }
     }
 }
